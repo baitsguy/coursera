4944a29b31b089df2af74fab4033e6e20b9563f8
Vinayak Thapliyal <vinayakthapliyal@gmail.com>
2017-09-17 20:23:42 -0700

    Added solution for multiGradientDescent

--- machine-learning/machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning/machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -6,7 +6,6 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -16,17 +15,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
-
+    inner = (X * theta) - y;
+    to_sum = repmat(inner, 1, size(X, 2)) .* X;
+    theta = theta - ((alpha/m) * sum(to_sum))';
     % ============================================================
 
     % Save the cost J in every iteration    

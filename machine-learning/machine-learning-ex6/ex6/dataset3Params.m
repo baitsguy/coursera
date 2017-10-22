a625e4c593ee56a55a3f0bf6f5662af6aaf505f8
Vinayak Thapliyal <vinayakthapliyal@gmail.com>
2017-10-23 01:09:04 +0530

    ex6: added sols for gaussianKernel.m and dataset3Params.m

--- machine-learning/machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning/machine-learning-ex6/ex6/dataset3Params.m
@@ -10,7 +10,6 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 % You need to return the following variables correctly.
 C = 1;
 sigma = 0.3;
-
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
@@ -21,14 +20,28 @@ sigma = 0.3;
 %
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
-%
+%A
 
 
+% err_g = 99999;
+% for i=[0.01 0.03 0.1 0.3 1 3 10 30]
+    % for j=[0.01 0.03 0.1 0.3 1 3 10 30]
+        % C_eval = i;
+        % sigma_eval = j;
+        % model = svmTrain(X, y, C_eval, @(x1, x2) gaussianKernel(x1, x2, sigma_eval));
+        % predict = svmPredict(model, Xval);
+        % err = mean(double(predict ~= yval));
+        % if (err < err_g)
+            % C = C_eval;
+            % sigma = sigma_eval;
+            % err_g = err;
+        % endif;
+    % end
+% end
 
-
-
-
-
+%hard coded for quicker submission
+C = 1;
+sigma = 0.1;
 % =========================================================================
 
 end
